b0d89cab987079aad21c4117769fbebfb29af968
Ankan Mandal <ankanmandal2001@gmail.com>
2024-11-16 15:46:39 +0530

    sign-up route in progress

---
+++ src/api/sign-up/route.ts
@@ -0,0 +1,11 @@
+import { resend } from "@/lib/resend";
+import { sendEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import { apiResponseTypes } from "@/types/apiResponseTypes";
+import UserModel from "@/model/user";
+import bcrypt from 'bcryptjs'
+
+export async function POST(request:Request){
+
+}
+
